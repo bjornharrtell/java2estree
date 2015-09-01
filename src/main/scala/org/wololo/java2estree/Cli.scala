6630a9c52a94bece048021859ffa4cd17ccfce3b
Björn Harrtell <bjorn@wololo.org>
2015-09-01 23:32:31 +0200

    Fix jar deps for executable Input from args

--- src/main/scala/org/wololo/java2estree/Cli.scala
+++ src/main/scala/org/wololo/java2estree/Cli.scala
@@ -1,49 +1,46 @@
 package org.wololo.java2estree
 
 import java.io.File
-import java.io.InputStreamReader
-import scala.collection.JavaConverters.seqAsJavaListConverter
+import java.io.FileReader
+
+import org.eclipse.jdt.core.dom.AST
+import org.eclipse.jdt.core.dom.ASTParser
+import org.eclipse.jdt.core.dom.CompilationUnit
+import org.eclipse.jface.text.Document
+
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.google.common.io.CharStreams
-import org.eclipse.jdt.core.JavaCore
-import java.nio.file.Files
-import java.io.FileReader
-import org.eclipse.jface.text.Document
-import org.eclipse.jdt.core.dom.ASTParser
-import org.eclipse.jdt.core.dom.AST
-import org.eclipse.jdt.core.dom.CompilationUnit
-import org.eclipse.jdt.core.dom.IVariableBinding
+import com.typesafe.scalalogging.LazyLogging
 
-object Cli extends App {
-  var filename = args(0)
-
-  val input = new File(filename)
-  // val base = filename.split("\\.")(0)
-  // val output = new File(base + ".ast")
+object Cli extends App with LazyLogging {
+  val path = new File(args(0)).toString()
+  val file = new File(args(1))
+  val filename = file.getName
   
-  val doc = new Document(CharStreams.toString(new FileReader(input)));
+  val doc = new Document(CharStreams.toString(new FileReader(file)));
   val parser = ASTParser.newParser(AST.JLS8)
   parser.setResolveBindings(true)
   parser.setBindingsRecovery(true)
   parser.setStatementsRecovery(true)
-  parser.setEnvironment(null, null, null, true)
-  parser.setUnitName("Coordinate.java")
+  var srcPaths = Array(path)
+  parser.setEnvironment(null, srcPaths, null, true)
+  parser.setUnitName(filename)
   parser.setSource(doc.get().toCharArray())
   val cu = parser.createAST(null).asInstanceOf[CompilationUnit]
   
-  val program = Converters.program(cu)
+  val program = Converters.toProgram(cu)
   val mapper = new ObjectMapper
   mapper.registerModule(DefaultScalaModule)
   mapper.enable(SerializationFeature.INDENT_OUTPUT)
-
-  //println(mapper.writeValueAsString(program))
   
-  val pb = new ProcessBuilder(List("./node_modules/astring/bin/astring", "--indent", "  ").asJava)
-  val p = pb.start()
+  mapper.writeValue(System.out, program)
+  
+  //val pb = new ProcessBuilder(List("./node_modules/astring/bin/astring", "--indent", "  ").asJava)
+  //val p = pb.start()
 
-  mapper.writeValue(p.getOutputStream, program)
+  //mapper.writeValue(p.getOutputStream, program)
 
-  println(CharStreams.toString(new InputStreamReader(p.getInputStream, "UTF-8")))
+  //println(CharStreams.toString(new InputStreamReader(p.getInputStream, "UTF-8")))
 }

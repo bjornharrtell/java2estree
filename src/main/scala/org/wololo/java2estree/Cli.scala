f62572075e8f039a21b10d75b802fd43abcaac09
Björn Harrtell <bjorn@wololo.org>
2015-09-02 21:01:49 +0200

    Cleanups

--- src/main/scala/org/wololo/java2estree/Cli.scala
+++ src/main/scala/org/wololo/java2estree/Cli.scala
@@ -15,19 +15,18 @@ import com.google.common.io.CharStreams
 import com.typesafe.scalalogging.LazyLogging
 
 object Cli extends App with LazyLogging {
-  val path = new File(args(0)).toString()
+  val path = new File(args(0)).toString
   val file = new File(args(1))
   val filename = file.getName
   
-  val doc = new Document(CharStreams.toString(new FileReader(file)));
+  val doc = new Document(CharStreams.toString(new FileReader(file)))
   val parser = ASTParser.newParser(AST.JLS8)
   parser.setResolveBindings(true)
   parser.setBindingsRecovery(true)
   parser.setStatementsRecovery(true)
-  var srcPaths = Array(path)
-  parser.setEnvironment(null, srcPaths, null, true)
+  parser.setEnvironment(null, Array(path), null, true)
   parser.setUnitName(filename)
-  parser.setSource(doc.get().toCharArray())
+  parser.setSource(doc.get.toCharArray)
   val cu = parser.createAST(null).asInstanceOf[CompilationUnit]
   
   val program = Converters.toProgram(cu)

2cc1c4ce091dad9dc8dbe802c4cfda56464eee39
Björn Harrtell <bjorn@wololo.org>
2016-02-19 08:02:49 +0100

    Add test script

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find /home/bjorn/code/jsts/src/org -type f -name "*.ast" -delete
+mvn package -DskipTests
+java -jar target/java2estree-0.1.0.jar /home/bjorn/code/jts/modules/core/src/main/java /home/bjorn/code/jsts/src
+for f in $(find /home/bjorn/code/jsts/src/org -name '*.ast')
+do
+  echo "Processing $f" 
+  ./node_modules/astring/bin/astring $f > "${f/%.ast/.js}"
+done
+
